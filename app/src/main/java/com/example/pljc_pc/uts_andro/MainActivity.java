a76aeda96096e12e86837e18555f67a6ab6d4210
kristinverosinaga12 <kristinsinaga69@gmail.com>
2019-02-07 21:24:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pljc_pc/uts_andro/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pljc_pc.uts_andro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
